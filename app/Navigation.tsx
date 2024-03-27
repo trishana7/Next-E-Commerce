575a1406a511a2f703fe332701fa417f39be2327
trishana <trishurai7@gmail.com>
2024-03-28 02:05:03 +0545

    feat: swiper js for slider and design

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -1,6 +1,14 @@
 "use client";
 import ProductCard from "./ProductCard";
 import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "./globals.css";
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
 export interface Product {
   id: number;
@@ -11,13 +19,17 @@ export interface Product {
 
 const Navigation: React.FC = (): JSX.Element => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSlideChange = (swiper: any) => {
+    setActiveIndex(swiper.realIndex);
+  };
 
   useEffect(() => {
     if (products.length === 0) {
-      fetch("https://mocki.io/v1/7f936757-c373-48a3-b727-0a0365f4726d")
+      fetch("https://mocki.io/v1/471450c6-9e39-49e0-8898-9b55ab8a6b63")
         .then((res) => res.json())
         .then((data: Product[]) => {
-          console.log(data);
           setProducts(data);
         })
         .catch((error) => console.error("Error fetching data:", error));
@@ -26,11 +38,49 @@ const Navigation: React.FC = (): JSX.Element => {
 
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <div className="bg-green-100/30 h-3/4 w-3/4 rounded-xl backdrop-blur-xl">
-        {Array.isArray(products) &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+      <div className="w-1/3 h-10 bg-green-200/30 backdrop-blur-lg mb-5 rounded-full"></div>
+
+      <div className="flex flex-col justify-center items-center bg-green-200/30 h-2/3 w-3/4 rounded-xl backdrop-blur-2xl">
+        {products.length !== 0 && (
+          <div className="flex justify-between items-center px-8 w-full">
+            <div className="flex gap-3">
+              <div className="w-10 h-10 bg-green-200 rounded-full"></div>
+              <div className="w-10 h-10 bg-green-200 rounded-full"></div>
+              <div className="w-10 h-10 bg-green-200 rounded-full"></div>
+            </div>
+            <div className="w-32 h-10 bg-green-200 rounded-full"></div>
+          </div>
+        )}
+        <div className=" max-w-lg">
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: false,
+            }}
+            pagination={true}
+            loop={true}
+            className="mySwiper"
+            onTransitionEnd={(swiper) => handleSlideChange(swiper)}
+          >
+            {Array.isArray(products) &&
+              products.map((product, index) => (
+                <SwiperSlide key={product.id}>
+                  <ProductCard
+                    key={product.id}
+                    product={product}
+                    isMiddleSlide={index === activeIndex}
+                  />
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -1,9 +1,46 @@
 import { Product } from "./Navigation";
-
-const ProductCard: React.FC<{ product: Product }> = ({ product }) => (
-  <div key={product.id}>
-    <h2>{product.name}</h2>
-    <p>{product.price}</p>
+import Image from "next/image";
+const handleAddToCart = (product: Product) => {
+  console.log("hello", product);
+};
+const ProductCard: React.FC<{ product: Product; isMiddleSlide: boolean }> = ({
+  product,
+  isMiddleSlide,
+}) => (
+  <div className="relative" key={product.id}>
+    <Image width={400} height={700} src={product.image} alt={product.name} />
+    <div
+      className={`${
+        isMiddleSlide
+          ? "absolute bg-white/20 -ml-10 p-4 rounded-full backdrop-blur-3xl duration-300 translate-y-10 justify-between w-80"
+          : "justify-center"
+      }  text-white flex items-center`}
+    >
+      <div>
+        <p
+          className={`${
+            !isMiddleSlide ? "text-center text-sm" : "text-base"
+          } font-medium`}
+        >
+          {product.name}
+        </p>
+        <p
+          className={`${
+            !isMiddleSlide ? "text-center  text-base" : "text-xl"
+          } font-bold`}
+        >
+          ${product.price}
+        </p>
+      </div>
+      {isMiddleSlide && (
+        <button
+          className="text-black text-sm bg-white p-2 rounded-full"
+          onClick={() => handleAddToCart(product)}
+        >
+          + Add to Cart
+        </button>
+      )}
+    </div>
   </div>
 );
 export default ProductCard;
